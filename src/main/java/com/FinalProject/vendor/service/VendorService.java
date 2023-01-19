c9db095fd0888f422d2c251bf22dfb192afc67f5
thapa <thapaliyan01@gmail.com>
2023-01-19 16:16:16 -0500

    Common Project

---
+++ src/main/java/com/FinalProject/vendor/service/VendorService.java
@@ -0,0 +1,15 @@
+package com.FinalProject.vendor.service;
+
+import com.FinalProject.vendor.repository.VendorRegRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VendorService {
+
+    @Autowired
+    VendorRegRepository vendorRegRepository;
+
+    public String save
+
+}
