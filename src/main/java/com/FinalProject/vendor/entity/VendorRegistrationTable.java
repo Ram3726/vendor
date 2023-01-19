bdf241f3bcf0b9a7090d7113531b5b77cc1e7121
Sundar Bashyal <sundarbashyal50@gmail.com>
2023-01-19 16:50:24 -0600

    corrction check

---
+++ src/main/java/com/FinalProject/vendor/entity/VendorRegistrationTable.java
@@ -0,0 +1,38 @@
+package com.FinalProject.vendor.entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Column;
+public class VendorRegistrationTable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+
+    @Column(name = "first_Name")
+    private String firstName;
+
+    @Column(name = "last_Name")
+    private String lastName;
+
+    @Column(name = "company_Name")
+    private String companyName;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone_Number")
+    private String phoneNumber;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "confirm_Password")
+    private String confirmPassword;
+
+    @Column(name = "operating_City")
+    private String operatingCity;
+
+}
--- src/main/java/com/FinalProject/vendor/service/VendorService.java
+++ src/main/java/com/FinalProject/vendor/service/VendorService.java
@@ -1,5 +1,9 @@
 package com.FinalProject.vendor.service;
 
+import com.FinalProject.vendor.controller.VendorRegController;
+import com.FinalProject.vendor.entity.CarTable;
+import com.FinalProject.vendor.entity.VendorRegistrationTable;
+import com.FinalProject.vendor.model.VendorRegModel;
 import com.FinalProject.vendor.repository.VendorRegRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,6 +14,28 @@ public class VendorService {
     @Autowired
     VendorRegRepository vendorRegRepository;
 
-    public String save
+    public String save;
+
+    public String saveVendor(VendorRegModel vendorRegModel) {
+        VendorRegistrationTable vendorRegistrationTable = new VendorRegistrationTable();
+        vendorRegistrationTable.setFirstName(vendorRegModel.getFirstName());
+        vendorRegistrationTable.setLastName(vendorRegModel.getLastName());
+        vendorRegistrationTable.setCompanyName(vendorRegModel.getCompanyName());
+        vendorRegistrationTable.setAddress(vendorRegModel.getAddress());
+        vendorRegistrationTable.setEmail(vendorRegModel.getEmail());
+        vendorRegistrationTable.setPhoneNumber(vendorRegModel.getPhoneNumber());
+        vendorRegistrationTable.setPassword(vendorRegModel.getPassword());
+        vendorRegistrationTable.setConfirmPassword(vendorRegModel.getConfirmPassword());
+        vendorRegistrationTable.setOperatingCity(vendorRegModel.getOperatingCity());
+
+        try {
+            vendorRegRepository.save(vendorRegistrationTable);
+        }catch (Exception e) {
+            System.err.println("Error details " + e.getMessage());
+
+        }
+
+        return "successful";
+    }
 
 }
