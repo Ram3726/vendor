214c8e4f6f5aaf201538ecf7af7ba00a1a3ff996
ramsu <ramsundar959@gmail.com>
2023-01-22 13:57:12 -0500

    Dashboard and vendorId added

--- src/main/java/com/FinalProject/vendor/repository/CarRepository.java
+++ src/main/java/com/FinalProject/vendor/repository/CarRepository.java
@@ -2,7 +2,17 @@ package com.FinalProject.vendor.repository;
 
 import com.FinalProject.vendor.entity.CarTable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CarRepository extends CrudRepository<CarTable,Integer> {
+    List<CarTable> findByVendorId(Integer vendorId);
+
+
+
+
+
+
 }
